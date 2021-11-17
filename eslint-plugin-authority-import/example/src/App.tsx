6aadc371358e1991db9cc5effc5b0a5ef3dde1b1
vb <vb@itech8.net>
2021-11-17 18:18:10 +0800

    vb

---
+++ eslint-plugin-authority-import/example/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Authority from './components/authority.component'
+import { myFn } from './vb'
+myFn()
+function App() {
+  return (
+    <div className="App">
+      <Authority />
+    </div>
+  );
+}
+
+export default App;
