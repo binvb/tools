9186ad83dfccd6c4dd4b896c8c85342c76c6fbea
vb <vb@itech8.net>
2021-12-17 14:22:56 +0800

    vb

--- koa-ts-prisma/router.ts
+++ koa-ts-prisma/router.ts
@@ -3,7 +3,15 @@ import { userController } from './src/controller/index'
 
 const router = new Router()
 
-router.get('/user', userController.getUser)
-router.post('/addUser', userController.addUser)
+// select
+router.get('/user', userController.getUser) // e.g. curl http://127.0.0.1:3001/user\?id\=1
+router.get('/userRaw', userController.getUserRaw) // e.g. curl http://127.0.0.1:3001/userRaw\?id\=1
+// add
+router.post('/addUser', userController.addUser) // e.g. curl -H "Content-Type: application/json" http://127.0.0.1:3001/addUser -X POST -d '{"email": "wenbin.vb@gmail.com", "name": "vb"}'
+router.post('/addUserRaw', userController.addUserRaw) // e.g. curl -H "Content-Type: application/json" http://127.0.0.1:3001/addUserRaw -X POST -d '{"email": "wenbin.vb1@gmail.com", "name": "vb1"}'
+// update
+router.put('/updateUser', userController.updateUser) // e.g. curl http://127.0.0.1:3001/updateUser -X PUT -d '{"email": "wenbin.vb3@gmail.com", "name": "vb3","id":3}' -H "Content-Type: application/json"
+// delete
+router.delete('/delUser', userController.delUser) // e.g. curl http://127.0.0.1:3001/delUser\?id\=4 -X DELETE
 
 export default router
--- koa-ts-prisma/src/controller/user.controller.ts
+++ koa-ts-prisma/src/controller/user.controller.ts
@@ -5,12 +5,17 @@ import { Context, Next } from 'koa'
 const prisma = new PrismaClient()
 class User {
   public static async getUser(ctx: Context, next: Next) {
-    console.log(ctx.query)
     let user = await prisma.user.findUnique({
       where: {
-        id: 1,
+        id: +(ctx.query.id as string),
       },
     })
+
+    ctx.status = 200
+    ctx.body = user
+  }
+  public static async getUserRaw(ctx: Context, next: Next) {
+    let user = await prisma.$queryRaw`select * from user where id=${ctx.query.id};`
 
     ctx.status = 200
     ctx.body = user
@@ -20,6 +25,33 @@ class User {
       data: {
         email: ctx.request.body.email as string,
         name: ctx.request.body.name as string
+      }
+    })
+    ctx.status = 200
+    ctx.body = ''
+  }
+  public static async addUserRaw(ctx:Context, next:Next) {
+    await prisma.$executeRaw`insert into user value(${null}, ${ctx.request.body.email}, ${ctx.request.body.name});`
+    ctx.status = 200
+    ctx.body = ''
+  }
+  public static async updateUser(ctx:Context, next:Next) {
+    await prisma.user.update({
+      where: {
+        id: ctx.request.body.id as number
+      },
+      data: {
+        email: ctx.request.body.email as string,
+        name: ctx.request.body.name as string
+      }
+    })
+    ctx.status = 200
+    ctx.body = ''
+  }
+  public static async delUser(ctx:Context, next:Next) {
+    await prisma.user.delete({
+      where: {
+        id: +(ctx.query.id as string)
       }
     })
     ctx.status = 200
